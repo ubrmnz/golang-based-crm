f6e973465cb2ae5407a97a27ca740c1b8ed0eef2
thecodemensch <zxvf@tutanota.com>
2022-02-01 22:23:44 +0530

    feat: added main.go

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
 	"github.com/thecodemensch/golang-based-crm/database"
+	"github.com/thecodemensch/golang-based-crm/lead"
 )
 
 func route_setups(app *fiber.App) {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLeads)
-	app.Delete(DeleteLeads)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
 }
 
 func init_database() {
-
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection established")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
